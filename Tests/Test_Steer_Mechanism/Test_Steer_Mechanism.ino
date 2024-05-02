d3edd70e8c099c382978786eeecc850e45e293e4
AndrewFerg05 <andrewferguson8@zoho.com>
2024-05-02 14:05:09 +0100

    Update Test_Steer_Mechanism.ino

--- Tests/Test_Steer_Mechanism/Test_Steer_Mechanism.ino
+++ Tests/Test_Steer_Mechanism/Test_Steer_Mechanism.ino
@@ -41,7 +41,7 @@
 // 13/04/24 - Known Issue: Mechianical steering issue means half lock left will be similar to full lock
 
 #include "BT_Comms.h"
-
+#include "CarControl.h"
 #define MOTOR_FORWARD_PIN 13
 #define MOTOR_REVERSE_PIN 12
 #define MOTOR_STEER_PIN 17
@@ -62,72 +62,11 @@ enum State
 enum State current_state = idle, next_state = idle;
 bool start_flag = 0;
 
-void forward(int dutyCyclePercentage) 
-{
-  // Convert duty cycle percentage to PWM duty cycle value (0-255)
-  int dutyCycleValue = map(dutyCyclePercentage, 0, 100, 0, 255);
-  
-  ledcWrite(0, dutyCycleValue); // Set PWM duty cycle
-}
-
-void reverse(int dutyCyclePercentage) 
-{
-  // Convert duty cycle percentage to PWM duty cycle value (0-255)
-  int dutyCycleValue = map(dutyCyclePercentage, 0, 100, 0, 255);
-  
-  ledcWrite(1, dutyCycleValue); // Set PWM duty cycle
-}
-
-void steer(int steering) 
-{
-    //Constrain steering angle input
-    steering = constrain(steering, -20, 20);
-    
-    int dutycycle;
-
-//  Straight = Duty Cycle 82
-//  Full lock left = Duty Cycle 71
-//  Full lock left = Duty Cycle 88
-
-    Serial.println(steering);
-    if (steering < 0) 
-    {
-        Serial.println("Lower");
-        // Map the range from -20 to 0 to the duty cycle range of 71 to 82 - full left steerin range
-        dutycycle = map(steering, -20, 0, 71, 82);
-    } 
-    else 
-    {
-        Serial.println("upper");
-        // Map the range from 0 to 20 to the duty cycle range of 82 to 88 - full right steering range
-        dutycycle = map(steering, 0, 20, 82, 88);
-    }
-    ledcWrite(3, dutycycle);  //Update servo PWM duty cycle
-}
-  
-
-
-void setupDrive()
-{
-//  Reverse
-  ledcAttachPin(MOTOR_REVERSE_PIN, 1);    // Attach PWM channel 1 to reverse motor pin
-  ledcSetup(1, DRIVE_PWM_FREQ, 8);        // PWM frequency: 10 Hz, PWM resolution: 8-bit (0-255)
-  ledcWrite(1, 0);                        // Set PWM duty cycle
-//  Forward
-  ledcAttachPin(MOTOR_FORWARD_PIN, 0);    // Attach PWM channel 0 to forward motor pin
-  ledcSetup(0, DRIVE_PWM_FREQ, 8);        // PWM frequency: 10 Hz, PWM resolution: 8-bit (0-255)
-  ledcWrite(0, 0);                        // Set PWM duty cycle 
-//  Steer
-  ledcAttachPin(MOTOR_STEER_PIN, 3);      // Attach PWM channel 2 to steering servo pin
-  ledcSetup(3, 50, 10);                   // Configure LEDC channel 2 with a frequency of 50Hz and a resolution of 8 bits
-  ledcWrite(3, 82);                       // Default to Straight     
-}
-
 
 void setup() 
 {
   setupDrive();
-//  setupBluetooth();
+  setupBluetooth();
   Serial.begin(115200);
 }
 
@@ -143,8 +82,7 @@ void loop()
           Serial.println();
           lastPrint = millis(); // Update lastPrint time
         }
-//        start_flag = getBluetoothFlag();
-        start_flag = 1;
+        start_flag = getBluetoothFlag();
         if(start_flag == 1)
         {
           next_state = drive;
@@ -163,62 +101,55 @@ void loop()
           lastPrint = millis(); // Update lastPrint time
         }
 
-        steer(20);
-        forward(75);
-        delay(1200);
-        forward(0);
-        delay(250);
         steer(-20);
-        reverse(75);
+        forward(100);
+        delay(1000);
+        forward(0);
+        delay(1000);
+        reverse(100);
         delay(1000);
         reverse(0);
-        while(1){}
-//        forward(0);
-//        delay(1000);
-//        reverse(100);
-//        delay(1000);
-//        reverse(0);
-//        delay(1000);
-//
-//        steer(-10);
-//        forward(100);
-//        delay(1000);
-//        forward(0);
-//        delay(1000);
-//        reverse(100);
-//        delay(1000);
-//        reverse(0);
-//        delay(1000);
-//                
-//        steer(0);
-//        forward(100);
-//        delay(1000);
-//        forward(0);
-//        delay(1000);
-//        reverse(100);
-//        delay(1000);
-//        reverse(0);
-//        delay(1000);
-//        
-//        steer(10);
-//        forward(100);
-//        delay(1000);
-//        forward(0);
-//        delay(1000);
-//        reverse(100);
-//        delay(1000);
-//        reverse(0);
-//        delay(1000);
-//
-//        steer(20);
-//        forward(100);
-//        delay(1000);
-//        forward(0);
-//        delay(1000);
-//        reverse(100);
-//        delay(1000);
-//        reverse(0);
-//        delay(1000);
+        delay(1000);
+
+        steer(-10);
+        forward(100);
+        delay(1000);
+        forward(0);
+        delay(1000);
+        reverse(100);
+        delay(1000);
+        reverse(0);
+        delay(1000);
+                
+        steer(0);
+        forward(100);
+        delay(1000);
+        forward(0);
+        delay(1000);
+        reverse(100);
+        delay(1000);
+        reverse(0);
+        delay(1000);
+        
+        steer(10);
+        forward(100);
+        delay(1000);
+        forward(0);
+        delay(1000);
+        reverse(100);
+        delay(1000);
+        reverse(0);
+        delay(1000);
+
+        steer(20);
+        forward(100);
+        delay(1000);
+        forward(0);
+        delay(1000);
+        reverse(100);
+        delay(1000);
+        reverse(0);
+        delay(1000);
         next_state = idle;
         break;
       }
